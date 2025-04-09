d170bdab477ac4978f9addfbf3044d6c77a9fab8
Muhammad Adnan <adnasir607@gmail.com>
2025-04-10 00:23:24 +0500

    user id is static rn

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ const App = () => {
           apiUrl: "http://localhost:5001",
           // "https://backend-vert-pi-68.vercel.app",
           apiKey: "mySecretKey123",
+          user_id: "67b6c518ce468cd40298a61e",
         });
       } else {
         console.error("Buyback widget failed to load.");
